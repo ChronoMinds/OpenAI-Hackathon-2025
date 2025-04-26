07a02765901f1a88685c0270c5a1ff257b5d8c20
Mark Wiesemann <wiesemann.m.o@gmail.com>
2025-04-26 10:42:07 +0000

    first step of merge - 2

---
+++ chrome_extension/content.js
@@ -0,0 +1,9 @@
+document.addEventListener("mouseup", () => {
+  const selectedText = window.getSelection().toString().trim();
+  if (selectedText.length > 0) {
+    chrome.runtime.sendMessage({
+      action: "textSelected",
+      text: selectedText
+    });
+  }
+});
